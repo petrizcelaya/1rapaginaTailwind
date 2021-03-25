4ed47a36a0798811e5c8698a6b21c932d0e9c13e
Petriz Celaya <celaya93_12@hotmail.com>
2021-03-24 20:04:20 -0600

    darkmode Tailwind

--- public/app.js
+++ public/app.js
@@ -3,3 +3,20 @@ let items = document.getElementById('items');
 menu.addEventListener('click',() => {
     items.classList.toggle('hidden');
 });
+
+const darkMode = document.getElementById('darkMode'); // checkbox
+const mode = document.getElementById('mode');
+const userConfig = window.matchMedia('(prefers-color-scheme: dark)');
+
+if (userConfig.matches) {
+    console.log('dark');
+    darkMode.checked = true;
+    mode.classList.toggle('dark');
+}else{
+    console.log('light');
+    darkMode.checked = false;
+}
+
+darkMode.addEventListener('click', ()=> {
+    mode.classList.toggle('dark');
+})
